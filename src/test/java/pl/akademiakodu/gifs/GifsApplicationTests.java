badba7516e8c4a4926d97bfc441c59bb2a9e8a1a
Julia Tomaszewska <rzechonek@gmail.com>
2020-02-16 11:00:48 +0100

    First commit

---
+++ src/test/java/pl/akademiakodu/gifs/GifsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.akademiakodu.gifs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
